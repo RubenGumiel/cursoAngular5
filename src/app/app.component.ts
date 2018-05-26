4218054a797a5330634f95f34b84eff319e426c9
Unknown <benchu.mza@gmail.com>
2018-05-26 01:55:17 -0300

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, DoCheck } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements DoCheck {
-  title = 'app';
+  title = 'NGZOO';
   email: string;
 
   ngDoCheck() {
